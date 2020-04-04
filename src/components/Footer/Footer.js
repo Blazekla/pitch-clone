7c56073a382d6f35eee2d5e1bd64c74f51459d36
Victor Leung <victorleung1119@gmail.com>
2020-04-04 17:32:42 -0500

    Correct the order of pitch and major column
    Put column 2, 3, 4 into a flex box named major column
    Set order of pitch to 2 and major column to 3
    Set calc in CSS for changing padding as the viewport size change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,22 @@
 import React from "react";
 import './Footer.css';
-import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 function Footer() {
   return (
-    <div className="">
+    <div>
       <Typography component="div">
         <Box
+          className="container"
           display="flex"
           justifyContent="center"
-          p={1} bgcolor="background.paper">
+          bgcolor="background.paper">
 
-          {/* First column */}
-          <Box display="flex" flexDirection="column" display="flex">
+
+          {/* Pitch column */}
+          <Box display="flex" flexGrow={2} flexDirection="column">
             <Box p={1} fontWeight="fontWeightBold">
               Pitch
             </Box>
@@ -25,66 +28,74 @@ function Footer() {
             </Box>
           </Box>
 
-          {/* Second column */}
-          <Box display="flex" flexDirection="column" display="flex">
-            <Box p={1} fontWeight="fontWeightBold">
-              Company
+          {/* Major column */}
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            p={1}
+            flexGrow={3}>
+
+            {/* Company Column */}
+            <Box display="flex" flexDirection="column">
+              <Box p={1} fontWeight="fontWeightBold">
+                Company
+              </Box>
+              <Box p={1}>
+                Home
+              </Box>
+              <Box p={1}>
+                About
+              </Box>
+              <Box p={1}>
+                Blog
+              </Box>
+              <Box p={1}>
+                Jobs
+              </Box>
+              <Box p={1}>
+                Press Room
+              </Box>
             </Box>
-            <Box p={1}>
-            Home
+
+
+            {/* Legal column */}
+            <Box display="flex" flexDirection="column">
+              <Box p={1} fontWeight="fontWeightBold">
+                Legal
+              </Box>
+              <Box p={1}>
+                Legal Notice
+              </Box>
+              <Box p={1}>
+                Privacy Policy
+              </Box>
+              <Box p={1}>
+                Terms of Use
+              </Box>
+              <Box p={1}>
+                DPA
+              </Box>
+              <Box p={1}>
+                ERDF Support
+              </Box>
             </Box>
-            <Box p={1}>
-            About
-            </Box>
-            <Box p={1}>
-            Blog
-            </Box>
-            <Box p={1}>
-            Jobs
-            </Box>
-            <Box p={1}>
-            Press Room
+
+            {/* Follow Us Column */}
+            <Box display="flex" flexDirection="column">
+              <Box p={1} fontWeight="fontWeightBold">
+                Follow us
+              </Box>
+              <Box p={1}>
+                SVG icons
+              </Box>
+              <Box p={1}>
+                Download a SVGICON Coloring Page
+              </Box>
+              <Box p={1}>
+                Download the PITCHSVGICON Game
+              </Box>
             </Box>
           </Box>
-
-
-          {/* Third column */}
-          <Box display="flex" flexDirection="column" display="flex">
-            <Box p={1} fontWeight="fontWeightBold">
-              Legal
-            </Box>
-            <Box p={1}>
-              Legal Notice
-            </Box>
-            <Box p={1}>
-              Privacy Policy
-            </Box>
-            <Box p={1}>
-              Terms of Use
-            </Box>
-            <Box p={1}>
-              DPA
-            </Box>
-            <Box p={1}>
-              ERDF Support
-            </Box>
-          </Box>
-
-          <Box display="flex" flexDirection="column" display="flex">
-            <Box p={1} fontWeight="fontWeightBold">
-              Follow us
-            </Box>
-            <Box p={1}>
-              SVG icons
-            </Box>
-            <Box p={1}>
-              Download a SVGICON Coloring Page
-            </Box>
-            <Box p={1}>
-              Download the PITCHSVGICON Game
-            </Box>
-          </Box>
-
         </Box>
       </Typography>
     </div>
