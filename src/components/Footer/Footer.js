9d4d80d07e51115e3d808dc2b05b41c438ab3028
Victor Leung <victorleung1119@gmail.com>
2020-04-04 16:22:42 -0500

    Add the basic structure of flexboxes in Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,92 @@
 import React from "react";
 import './Footer.css';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 function Footer() {
   return (
     <div className="">
-    Footer
+      <Typography component="div">
+        <Box
+          display="flex"
+          justifyContent="center"
+          p={1} bgcolor="background.paper">
+
+          {/* First column */}
+          <Box display="flex" flexDirection="column" display="flex">
+            <Box p={1} fontWeight="fontWeightBold">
+              Pitch
+            </Box>
+            <Box p={1}>
+            © Copyright 2020 Pitch Software GmbH.
+            </Box>
+            <Box p={1}>
+            All rights reserved.
+            </Box>
+          </Box>
+
+          {/* Second column */}
+          <Box display="flex" flexDirection="column" display="flex">
+            <Box p={1} fontWeight="fontWeightBold">
+              Company
+            </Box>
+            <Box p={1}>
+            Home
+            </Box>
+            <Box p={1}>
+            About
+            </Box>
+            <Box p={1}>
+            Blog
+            </Box>
+            <Box p={1}>
+            Jobs
+            </Box>
+            <Box p={1}>
+            Press Room
+            </Box>
+          </Box>
+
+
+          {/* Third column */}
+          <Box display="flex" flexDirection="column" display="flex">
+            <Box p={1} fontWeight="fontWeightBold">
+              Legal
+            </Box>
+            <Box p={1}>
+              Legal Notice
+            </Box>
+            <Box p={1}>
+              Privacy Policy
+            </Box>
+            <Box p={1}>
+              Terms of Use
+            </Box>
+            <Box p={1}>
+              DPA
+            </Box>
+            <Box p={1}>
+              ERDF Support
+            </Box>
+          </Box>
+
+          <Box display="flex" flexDirection="column" display="flex">
+            <Box p={1} fontWeight="fontWeightBold">
+              Follow us
+            </Box>
+            <Box p={1}>
+              SVG icons
+            </Box>
+            <Box p={1}>
+              Download a SVGICON Coloring Page
+            </Box>
+            <Box p={1}>
+              Download the PITCHSVGICON Game
+            </Box>
+          </Box>
+
+        </Box>
+      </Typography>
     </div>
 );
 }
