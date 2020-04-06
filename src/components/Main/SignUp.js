2d193e96d9faa09eca097858c209704d472575bc
Blazekla <cristlui4@gmail.com>
2020-04-05 22:16:00 -0500

    added signup component

---
+++ src/components/Main/SignUp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TextFiled from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+
+function SignUp() {
+  return (
+    <Grid container>
+      <Grid item>
+        <TextField />
+        <Button></Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default SignUp;
