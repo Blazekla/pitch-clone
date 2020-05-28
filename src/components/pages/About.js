ff131366db4cc7b1de7a016baf27edfe21161e85
Victor Leung <victorleung1119@gmail.com>
2020-05-27 20:18:26 -0500

    Make the mobile links functional
    1. Make the mobile links functional.
    2. Create function and state to change active button class on different pages.
    3. Delete the extra burger menu button. Make the only burger button 'display: none' at large viewport

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,33 @@
 import React from "react";
-
+import './About.css';
 
 function About() {
+
   return (
     <div>
+      <div className='parallax'>
+
+        <div className='parallax-pic'>It all starts with an Idea</div>
+      </div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
+      <div>This is the About page</div>
       <div>This is the About page</div>
       <div>This is the About page</div>
       <div>This is the About page</div>
